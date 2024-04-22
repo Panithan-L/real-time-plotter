79c4ce376a3d56a069fee6f6d9c88e43051febbe
Panithan Lertsuntivit <panithanblue@gmail.com>
2024-04-22 00:59:00 -0700

    Add template file

---
+++ ESP32-Template.c
@@ -0,0 +1,25 @@
+
+//Header Files
+#include <stdio.h>
+#include "driver\gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+//Function Prototypes
+
+//Defining Variables
+
+
+void app_main(void)
+{
+    //Variable Declaration
+
+    //Data Input - set values for variables
+
+    //Configure Digital I/O for Stepper Motor
+
+
+
+}
+
+//Functions
